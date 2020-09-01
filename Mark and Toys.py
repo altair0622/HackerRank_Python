4a5ba2ccfc241844f1825dccadc00147e885cc06
Han Kim <51448660+altair0622@users.noreply.github.com>
2020-09-01 15:19:00 -0400

    Create Mark and Toys.py

---
+++ Mark and Toys.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def merge_sort(values): 
+  
+    if len(values)>1: 
+        m = len(values)//2
+        left = values[:m] 
+        right = values[m:] 
+        left = merge_sort(left) 
+        right = merge_sort(right) 
+  
+        values =[] 
+  
+        while len(left)>0 and len(right)>0: 
+            if left[0]<right[0]: 
+                values.append(left[0]) 
+                left.pop(0) 
+            else: 
+                values.append(right[0]) 
+                right.pop(0) 
+  
+        for i in left: 
+            values.append(i) 
+        for i in right: 
+            values.append(i) 
+                  
+    return values
+
+def maximumToys(arr, k):
+    arr = merge_sort(arr) # replace with arr.sort()
+    
+    count = 0
+    for ele in arr:
+        k = k-ele
+        count+=1
+        if k < 0 :
+            return count-1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
