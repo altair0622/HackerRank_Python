00608088674d3f5b19cb8dab0f58cfca72255074
Han Kim <51448660+altair0622@users.noreply.github.com>
2020-09-24 21:19:19 -0400

    Create Chocolate Feast.py

---
+++ Chocolate Feast.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the chocolateFeast function below.
+def chocolateFeast(n, c, m):
+    choco = int(n / c)
+    if choco >= m:
+        wrapper = choco
+        while wrapper > 0:
+            wrapper = wrapper - m
+            if wrapper >= 0:
+                choco += 1
+                wrapper += 1
+    return choco
+'''
+choco = n//c
+wrapper = choco
+while wrapper>=m:
+    a,b=divmod(wrapper, m)
+    choco += a
+    wapper = a + b
+return choco
+'''
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        ncm = input().split()
+
+        n = int(ncm[0])
+
+        c = int(ncm[1])
+
+        m = int(ncm[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
