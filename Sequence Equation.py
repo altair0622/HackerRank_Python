764079758f5b607a4d9f72a7231eaea14411b191
Han Kim <51448660+altair0622@users.noreply.github.com>
2020-09-28 13:51:39 -0400

    Create Sequence Equation.py

---
+++ Sequence Equation.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the permutationEquation function below.
+def permutationEquation(p):
+    l= []
+    for i in range(len(p)):
+        first=p.index(i+1)
+        second = p.index(first+1)
+        l.append(second+1)
+    return l
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
